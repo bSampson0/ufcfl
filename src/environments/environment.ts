856a1cd56783bda598cfe3dbc68771e5cbb7b47d
Bryce S <bnicerrr@gmail.com>
2023-08-25 12:39:44 -0700

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: "",
-    authDomain: "",
-    databaseURL: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: ""
+    apiKey: "AIzaSyD3Cwm9EUkf4M0NtqDXPz8nfwZzkFpwHdg",
+    authDomain: "ufc-fantasy-350a5.firebaseapp.com",
+    databaseURL: "https://ufc-fantasy-350a5-default-rtdb.firebaseio.com",
+    projectId: "ufc-fantasy-350a5",
+    storageBucket: "ufc-fantasy-350a5.appspot.com",
+    messagingSenderId: "711964575969",
+    appId: "1:711964575969:web:819fe33ee5d28739b8cfa0"
   }
 };
